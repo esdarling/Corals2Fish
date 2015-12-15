7acf85cb01200d4298e64f634e4c6a4a10843658
Emily Darling <esdarling@gmail.com>
2015-12-15 15:46:17 -0500

    plots

--- analysis.R
+++ analysis.R
@@ -37,4 +37,89 @@ head(Zall)
 corvif(Zall)        
 
 
+# Fish results plots
+min(dat$Complexity); max(dat$Complexity)
+#abundance
+ggplot(data = dat, aes(x = Complexity, y = FishAbundance)) +
+  geom_point(aes(fill = Habitat), shape = 21, size = 4, 
+             colour = "darkgrey", alpha = 0.8) + 
+  stat_smooth(method = "lm", se = TRUE) +
+  stat_smooth(method = "loess", se = TRUE, colour = "red") +
+  scale_fill_manual(values = c("grey10","grey70", "white")) +
+  theme_bw(base_size = 20) +
+  scale_x_continuous(limits = c(0.5,3.69), expand = c(0.01,0.01)) +
+  xlab("Structural complexity") +
+  ylab("Abundance") +
+  theme(legend.key = element_blank(), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) 
 
+setwd("/Users/emilydarling/Documents/Work/GitHub/Corals2Fish/1_plots")  
+ggsave("abundance plot.pdf", height= 3, width = 6, units = "in")    
+
+#biomass
+dat$logFishBiomass <- log10(dat$FishBiomass)   
+min(dat$logFishBiomass); max(dat$logFishBiomass)
+
+ggplot(data = dat, aes(x = Complexity, y = logFishBiomass)) +
+  geom_point(aes(fill = Management), shape = 21, size = 4, 
+             colour = "darkgrey", alpha = 0.8) + 
+  stat_smooth(method = "lm", se = TRUE) +
+  stat_smooth(method = "loess", se = TRUE, colour = "red") +
+  scale_fill_manual(values = c("white","grey10")) +
+  theme_bw(base_size = 19) +
+  scale_x_continuous(limits = c(0.5,3.69), expand = c(0.01,0.01)) +
+  scale_y_continuous(limits = c(1.5,4.5), breaks = c(2,3,4)) +
+  xlab("Structural complexity") +
+  ylab(expression(paste("log(Biomass, kg ", ha^-1,")"))) +
+  theme(legend.key = element_blank(), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) 
+
+setwd("/Users/emilydarling/Documents/Work/GitHub/Corals2Fish/1_plots")  
+ggsave("biomass plot.pdf", height= 3, width = 6.8, units = "in")    
+
+#diversity
+ggplot(data = dat, aes(x = Complexity, y = FishDiversity)) +
+  geom_point(aes(fill = Habitat), shape = 21, size = 4, 
+             colour = "darkgrey", alpha = 0.8) + 
+  stat_smooth(method = "lm", se = TRUE) +
+  stat_smooth(method = "loess", se = TRUE, colour = "red") +
+  scale_fill_manual(values = c("grey10","grey70", "white")) +
+  theme_bw(base_size = 19) +
+  scale_x_continuous(limits = c(0.5,3.69), expand = c(0.01,0.01)) +
+  xlab("Structural complexity") +
+  ylab("Species richness") +
+  theme(legend.key = element_blank(), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) 
+
+setwd("/Users/emilydarling/Documents/Work/GitHub/Corals2Fish/1_plots")  
+ggsave("diversity plot.pdf", height= 3.2, width = 6, units = "in")   
+
+## predictors of complexity models
+names(dat)
+dat_melt2 <- melt(dat[,c(4,8,12,33:37,29,24,31,27,9)], id.vars = 1:3, variable.name = "CoralResponse") 
+head(dat_melt2) 
+levels(dat_melt2$CoralResponse)
+
+dat_melt2$CoralResponse <- factor(dat_melt2$CoralResponse, 
+                                  levels = c("StressTolerant","Competitive",
+                                            "no_genera","Generalist",
+                                            "CWMMaxsize","Weedy",
+                                            "CWMBrooding","Depth_m",
+                                            "CWMFecundity","CWMBranching"))
+
+ggplot(data = dat_melt2, aes(x = value, y = Complexity)) +
+  geom_point(shape = 21, alpha = 0.45, size = 3,
+             colour = "black", fill = "grey90") + 
+  stat_smooth(method = "lm", se = TRUE) +
+  stat_smooth(method = "loess", se = TRUE, colour = "red") +
+  facet_wrap(~ CoralResponse, scales = "free", ncol =3) +
+  theme_bw(base_size = 19) +
+  ylab("Structural complexity") +
+  theme(legend.key = element_blank(), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank())     
+setwd("/Users/emilydarling/Documents/Work/GitHub/Corals2Fish/1_plots")  
+ggsave("complex multiplot.pdf", height= 10, width = 10, units = "in") 
