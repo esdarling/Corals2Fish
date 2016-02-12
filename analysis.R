9ca1431f991d53beafb26a54633e606775cbce19
Emily Darling <esdarling@gmail.com>
2016-02-12 10:08:04 -0500

    update after indo

--- analysis.R
+++ analysis.R
@@ -3,7 +3,12 @@
 # ====================================
 setwd("/Users/emilydarling/Dropbox/1-On the go/Graham_FD test/Data")         
 dat <- read.csv("Graham_coralfish_25Sept2014.csv", header = TRUE, stringsAsFactors = TRUE) 
-names(dat)    
+names(dat)   
+
+table(with(dat, Country))
+
+min(dat$FishBiomass)
+max(dat$FishBiomass)
 
 unique(dat$Year)
 unique(dat$Country)
@@ -18,6 +23,7 @@ with(dat, table(Country, Habitat))
 
 
 #consider VIFs 
+setwd("/Users/emilydarling/Documents/Work/GitHub/Corals2Fish")  
 source("HighstatLib.R")  
 
 
@@ -28,6 +34,14 @@ corvif(Z5)
 
 Z5b <- dat[,c("total_cover","Competitive","StressTolerant","Generalist","Weedy")]
 corvif(Z5b)
+
+#VIFS if coral cover and life histories included in model
+Zall <- dat[,c("Complexity","no_genera","Rao",
+               "total_cover",
+               "CWMBranching","CWMMaxsize","CWMBrooding","CWMFecundity",
+               "Competitive","StressTolerant","Generalist","Weedy")]  
+head(Zall)
+corvif(Zall)    
 
 #Final set for life history models
 Zall <- dat[,c("Complexity","no_genera","Rao",
@@ -99,27 +113,57 @@ ggsave("diversity plot.pdf", height= 3.2, width = 6, units = "in")
 
 ## predictors of complexity models
 names(dat)
-dat_melt2 <- melt(dat[,c(4,8,12,33:37,29,24,31,27,9)], id.vars = 1:3, variable.name = "CoralResponse") 
+dat_melt2 <- melt(dat[,c(4,8,12,10,33:37,29,24,31,27,9)], id.vars = 1:3, variable.name = "CoralResponse") 
 head(dat_melt2) 
 levels(dat_melt2$CoralResponse)
 
+#add a dummy column blank in for reef zone/habitat
+#recode facet labels
+dat_melt2$CoralResponse <- recode(dat_melt2$CoralResponse, 
+                                  "'no_genera' = 'Genera richness';
+'CWMMaxsize' = 'Colony size'; 'CWMBrooding' = 'Brooding'; 'Depth_m' = 'Depth';
+'CWMFecundity' = 'Fecundity'; 'CWMBranching' = 'Branching'")
+
 dat_melt2$CoralResponse <- factor(dat_melt2$CoralResponse, 
-                                  levels = c("StressTolerant","Competitive",
-                                            "no_genera","Generalist",
-                                            "CWMMaxsize","Weedy",
-                                            "CWMBrooding","Depth_m",
-                                            "CWMFecundity","CWMBranching"))
+                                  levels = c("Latitude","StressTolerant","Competitive",
+                                            "Genera richness","Generalist",
+                                            "Colony size","Weedy",
+                                            "Brooding","Depth",
+                                            "Fecundity","Branching"))
+levels(dat_melt2$CoralResponse)
 
 ggplot(data = dat_melt2, aes(x = value, y = Complexity)) +
   geom_point(shape = 21, alpha = 0.45, size = 3,
              colour = "black", fill = "grey90") + 
   stat_smooth(method = "lm", se = TRUE) +
   stat_smooth(method = "loess", se = TRUE, colour = "red") +
+  scale_y_continuous(limits = c(0.5,4.5)) +
   facet_wrap(~ CoralResponse, scales = "free", ncol =3) +
   theme_bw(base_size = 19) +
   ylab("Structural complexity") +
   theme(legend.key = element_blank(), 
         panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank())     
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank())      
 setwd("/Users/emilydarling/Documents/Work/GitHub/Corals2Fish/1_plots")  
-ggsave("complex multiplot.pdf", height= 10, width = 10, units = "in") 
+ggsave("complex multiplot_withdummy.pdf", height= 10, width = 10, units = "in") 
+
+#reef zone/habitat
+head(dat)
+names(dat)
+
+levels(dat$Habitat)
+dat$Habitat <- factor(dat$Habitat, levels = c("Slope","Crest","Flat"))
+
+ggplot(data = dat, aes(x = Habitat, y = Complexity)) +
+  geom_boxplot() + 
+  theme_bw(base_size = 19) +
+  scale_y_continuous(limits = c(0.5,4.5)) +
+  theme(legend.key = element_blank(), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank()) 
+setwd("/Users/emilydarling/Documents/Work/GitHub/Corals2Fish/1_plots")  
+ggsave("habitat_for complex multiplot.pdf", height= 2.5, width = 3.3, units = "in") 
